e3e8b06e05b20fd04738bcefd992987b4f475bb8
FernandoRossiPythonm <fernando.cgr@gmail.com>
2023-02-27 15:05:02 -0300

    atualizacao

--- filme/apps.py
+++ filme/apps.py
@@ -5,6 +5,7 @@ class FilmeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'filme'
 
+
     def ready(self):
         from .models import Usuario
         import os
